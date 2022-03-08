12ce73c09bc69d6e23047ca2d089930140702936
= <=>
2022-03-07 23:45:41 -0500

    Modifying Fractal Inputs and Parameters

--- src/components/FractalGenerator.tsx
+++ src/components/FractalGenerator.tsx
@@ -13,37 +13,42 @@ export const FractalCanvas = () => {
   var canvasObj = canvasRef.current;
   var ctx = canvasObj?.getContext('2d');
 
-  var [maxIterations, setMaxIterations] = useState(13);
-  var [magnificationFactor, setMagnifictionFactor] = useState(250);
-  var [panX, setPanX] = useState(2.25);
-  var [panY, setPanY] = useState(1.2);
+  const focusPoint = 0;
 
+  const defaultMagnification = 250.0;
+  const defaultPanX = 2.25;
+  const defaultPanY = 1.2;
+
+  var [maxIterations, setMaxIterations] = useState('13');
+  var [magnificationFactor, setMagnifictionFactor] = useState(String(defaultMagnification));
+  var [panX, setPanX] = useState(String(defaultPanX));
+  var [panY, setPanY] = useState(String(defaultPanY));
+  
+  var [xOffset, setXOffset] = useState('0');
+  var [yOffset,setYOffset] = useState('0');
+  
+  var [boundry, setBoundry] = useState('5');
   var [inverse, setInverse] = useState(true);
-  var [boundry, setBoundry] = useState(5);
 
   useMountEffect(setup_canvas);
 
   useEffect(() => {
       draw_canvas();
- }, [maxIterations, magnificationFactor, panX, panY, inverse]);
+ }, [maxIterations, magnificationFactor, panX, panY, inverse, xOffset, yOffset]);
 
  
   function setup_canvas() {
     canvasObj = canvasRef?.current;
     ctx = canvasObj?.getContext('2d');
 
-    if (canvasRef == null) return;
     if (canvasObj == null) return;
-    if (ctx == null) return;
 
     canvasObj.width=800;
     canvasObj.height=600;
 
-    clear_canvas();
   }
 
   function clear_canvas() {
-    if (canvasRef == null) return;
     if (canvasObj == null) return;
     if (ctx == null) return;
     ctx.clearRect(0, 0, canvasObj.width, canvasObj.height)
@@ -51,6 +56,9 @@ export const FractalCanvas = () => {
 
   function draw_canvas() {
     
+
+    console.log("draw_canvas(); %f", magnificationFactor);
+
     clear_canvas();
 
     if (canvasObj == null) return;
@@ -60,13 +68,8 @@ export const FractalCanvas = () => {
     for(var x=0; x < canvasObj.width; x++) {
       for(var y=0; y < canvasObj.height; y++) {
 
-        var mag_x =  x/magnificationFactor - panX;
-        var mag_y =  y/magnificationFactor - panY;
-        
-        // var belongsToSet = checkIfBelongsToMandelbrotSet(mag_x, mag_y);        
-        // if(belongsToSet == true) {
-        //   ctx.fillRect(x,y, 1,1); 
-        // }   
+        var mag_x =  x/Number(magnificationFactor) - Number(panX);
+        var mag_y =  y/Number(magnificationFactor) - Number(panY);
         
         var belongsToSet = checkIfBelongsToMandelbrotSet2(mag_x, mag_y);        
         if(belongsToSet == 0) {
@@ -81,19 +84,14 @@ export const FractalCanvas = () => {
 }
 
   function checkIfBelongsToMandelbrotSet2 (x: number, y: number): Number{
-    // var realComponentOfResult: Number = x;
-    // var imaginaryComponentOfResult: Number = y;
 
     var real: number = x;
     var imaginary: number = y;
 
 
-
-    for(var i = 0; i < maxIterations; i++) {
-      // Calculate the real and imaginary components of the result
-      // separately
+    for(var i = 0; i < Number(maxIterations); i++) {
+      // Calculate the real and imaginary components of the result separately
       var tempRealComponent = real * real - imaginary * imaginary + x;
-
       var tempImaginaryComponent = 2 * real * imaginary + y;
 
       real = tempRealComponent;
@@ -102,22 +100,44 @@ export const FractalCanvas = () => {
 
     // In the Mandelbrot set
     if (inverse) {
-      if (real * imaginary > boundry)
-      return i / maxIterations * 100.00;
+      if (real * imaginary > Number(boundry))
+      return i / Number(maxIterations) * 100.00;
     }
     else {
-      if (real * imaginary < boundry)
-        return i / maxIterations * 100.00;
+      if (real * imaginary < Number(boundry))
+        return i / Number(maxIterations) * 100.00;
     }
 
     return 0; // Not in the set
   }
 
- 
 
-  function focus (event: any) {
-    
+
+  function focusEvent (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
+
+    // function focus (event: MouseEvent) {
+
+    // console.log(event.clientX);
+    // console.log(event.offsetX);
+
+    var wmin = 0;
+    var hmin = 0;
+    var wmax = canvasObj?.width;
+    var hmax = canvasObj?.height;
+
+    // panX = event.offsetX;
+    // panY = event.offsetY;
+
+    focus();
   }
+
+  function focus() {
+    var multiplier: number  = (defaultMagnification / Number(magnificationFactor))
+
+    setPanX(String(defaultPanX * multiplier + Number(xOffset)));    
+    setPanY(String(defaultPanY * multiplier + Number(yOffset)));
+  }
+
 
 
   
@@ -129,39 +149,49 @@ export const FractalCanvas = () => {
 
       <div className="attribute"> 
           <label>Max Iterations</label> 
-          <input type='input' defaultValue={maxIterations} onInput={event => setMaxIterations(Number(event.currentTarget.value))}/> 
+          <input type='input' defaultValue={maxIterations} value={ maxIterations } onInput={event => setMaxIterations(event.currentTarget.value)}/> 
         </div>
 
         <div className="attribute"> 
           <label>Magnification Factor</label> 
-          <input type='input' defaultValue={magnificationFactor} onInput={event => setMagnifictionFactor(Number(event.currentTarget.value))}/> 
+          <input type='input' defaultValue={magnificationFactor} value={ magnificationFactor } onInput={event => setMagnifictionFactor(event.currentTarget.value)}/> 
         </div>
         
         <div className="attribute"> 
           <label>Pan X</label> 
-          <input type='input' defaultValue={panX} onInput={event => setPanX(Number(event.currentTarget.value))}/> 
+          <input type='input' defaultValue={panX} value={ panX } onInput={event => setPanX(event.currentTarget.value)}/> 
         </div>
         
         <div className="attribute"> 
           <label>Pan Y</label> 
-          <input type='input' defaultValue={panY} onInput={event => setPanY(Number(event.currentTarget.value))}/> 
+          <input type='input' defaultValue={panY} value={ panY } onInput={event => setPanY(event.currentTarget.value)}/> 
+        </div>
+        
+        <div className="attribute"> 
+          <label>X Offset</label> 
+          <input type='input' defaultValue={xOffset} value={ xOffset } onInput={event => setXOffset(event.currentTarget.value)}/> 
+        </div>
+        
+        <div className="attribute"> 
+          <label>Y Offset</label> 
+          <input type='input' defaultValue={yOffset} value={ yOffset } onInput={event => setYOffset(event.currentTarget.value)}/> 
         </div>
 
         <div className="attribute"> 
           <label>Inverse</label> 
-          <input type='checkbox' checked={inverse} onChange={event => setInverse(!inverse)}/> 
+          <input type='checkbox' checked={inverse} onInput={event => setInverse(!inverse)}/> 
         </div>
 
         <div className="attribute"> 
           <label>Boundry</label> 
-          <input type='input' defaultValue={boundry} onInput={event => setBoundry(Number(event.currentTarget.value))}/> 
+          <input type='input' defaultValue={boundry} value={ boundry } onInput={event => setBoundry(event.currentTarget.value)}/> 
         </div>
         
-        <button onClick={ event => draw_canvas() }>Draw Canvas</button>
+        <button onClick={ event => draw_canvas() } >Draw Canvas</button>
       </div>
 
       <div id="canvas-container">
-        <canvas id="canvas" ref={ canvasRef } onMouseMove={ event => focus(event) }></canvas>
+        <canvas id="canvas" ref={ canvasRef } onMouseMove={ event => focusEvent(event) }></canvas>
       </div>
 
       <div>
